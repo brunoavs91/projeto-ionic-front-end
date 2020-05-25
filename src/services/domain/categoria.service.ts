0265d365b3b8fc21accc2b964c5a0938f4ded40b
brunoav91 <brunoav91@gmail.com>
2020-05-24 21:13:47 -0300

    get categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from 'src/config/api.config';
+import { CategoriaDTO } from 'src/models/categoria.dto';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CategoriaService{
+
+    constructor(public http: HttpClient){
+
+    }
+
+    findAll() : Observable<CategoriaDTO[]> {
+        console.log(this.http.get("http://localhost:8080/categorias"));
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+}
