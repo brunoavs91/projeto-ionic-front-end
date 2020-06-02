a7e7ec8245ddda61576ae6e9d3291ad7138b21b7
brunoav91 <brunoav91@gmail.com>
2020-06-02 12:09:33 -0300

    Inserindo buscar categorias

--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { API_CONFIG } from 'src/config/api.config';
 import { CategoriaDTO } from 'src/models/categoria.dto';
 import { Observable } from 'rxjs';
@@ -7,12 +7,18 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class CategoriaService{
 
-    constructor(public http: HttpClient){
+    token :any;
 
+    constructor(public http: HttpClient){
+        this.token ='eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJicnVub2F2OTFAZ21haWwuY29tIiwiZXhwIjoxNTkwNDQ3Mjc4fQ.NYD7XhgeqcKpsa4Rc8Pb4cpwSWOuFaQi1Q2AEwelRj-kZ6_PVFyv1Cv0028_xe1AQ-kTfTYXkzNY1n1rTWFsgQ'
     }
 
     findAll() : Observable<CategoriaDTO[]> {
-        console.log(this.http.get("http://localhost:8080/categorias"));
-        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
-    }
+
+ 
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`, {headers: new HttpHeaders({'Authorization': 'Bearer ' + this.token})});
+        
+    }   
 }
