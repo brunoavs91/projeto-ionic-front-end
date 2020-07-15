b082909c2f8f958d11d6e3ff8f702500377281df
brunoav91@gmail.com <brunoav91@gmail.com>
2020-07-14 23:31:03 -0300

    tratando erros

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,17 +3,18 @@ import { Observable, throwError } from 'rxjs';
 import { Injectable } from '@angular/core'
 import { catchError, retry } from 'rxjs/operators';
 import { StorageService } from 'src/services/storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage : StorageService){}
+    constructor(public storage: StorageService, public alertCtrl: AlertController) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
             .pipe(
                 retry(1),
                 catchError((error: HttpErrorResponse) => {
-                    console.log("passou");
+
                     let errorMessage = '';
                     if (error.error instanceof ErrorEvent) {
                         // client-side error
@@ -25,9 +26,15 @@ export class ErrorInterceptor implements HttpInterceptor {
                     console.log(errorMessage);
 
                     switch (error.status) {
-                        case 403:
-                            this.handle403();
+                        case 401:
+                            this.handle401();
                             break;
+
+                        case 403:
+                            this.handle401();
+                            break;
+                        default:
+                            this.handleDefault(error);
 
                     }
                     return throwError(errorMessage);
@@ -35,8 +42,38 @@ export class ErrorInterceptor implements HttpInterceptor {
             )
     }
 
+    handleDefault(error) {
+        let alert = this.alertCtrl.create({
+            header: 'Error' + error.status + ' : ' + error,
+            message: error.message,
+            backdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.then(alert => alert.present());
+    }
+
+    
+
     handle403() {
-    this.storage.setLocalUser(null);
+        this.storage.setLocalUser(null);
+    }
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            header: 'Error',
+            message: 'Email ou senha incorretos',
+            backdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.then(alert => alert.present());
     }
 
 }
