ae51e88127f3df06a95e11731b507180fcb0ad3d
brunoav91@gmail.com <brunoav91@gmail.com>
2020-08-05 23:57:22 -0300

    Produtos em andamento

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from 'src/services/domain/categoria.service';
 import { CategoriaDTO } from 'src/models/categoria.dto';
 import { API_CONFIG } from 'src/config/api.config';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-categorias',
@@ -13,7 +14,8 @@ export class CategoriasPage implements OnInit {
   bucketUrl : string = API_CONFIG.bucketBaseUrl;
   items : CategoriaDTO[];
   
-  constructor(public categoriaService : CategoriaService) { }
+  constructor(public categoriaService : CategoriaService,
+    public navCtrl : NavController) { }
 
 
 
@@ -24,6 +26,14 @@ export class CategoriasPage implements OnInit {
       
     },
     error =>{});
+  }
+
+  showProdutos(){
+    this.navCtrl.navigateRoot("produtos");
+    //this.navCtrl.navigateBack("categorias");
+  }
+  backCategorias(){
+    this.navCtrl.navigateRoot("categorias");
   }
 
   ionViewDidLoad() {
---
+++ src/app/pages/produtos/produtos.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProdutoDTO } from 'src/models/produto.dto';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.page.html',
+  styleUrls: ['./produtos.page.scss'],
+})
+export class ProdutosPage implements OnInit {
+
+  items :ProdutoDTO [];
+  constructor(public navCtrl : NavController) { }
+
+  ngOnInit() {
+    this.items =[{
+      id : "1",
+      nome : "Mouse",
+      preco : 80.99
+    },
+  {
+    id : "2",
+    nome : "Teclado",
+    preco : 100.00
+  }];
+  }
+
+  backCategorias(){
+    this.navCtrl.navigateRoot("categorias");
+  }
+
+}
